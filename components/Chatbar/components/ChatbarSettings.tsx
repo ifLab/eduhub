09dce10ac72955ed9cd272b87bfc30c13422d93f
Wang Yiwen <105685809+proarn@users.noreply.github.com>
2023-08-21 09:48:57 +0000

    Toggle Button for Left Sidebar

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -1,6 +1,4 @@
-// import { IconFileExport, IconSettings, IconUsers, IconUser, IconWorldUpload, IconFileDescription, IconMathFunction } from '@tabler/icons-react';
-import { IconFileExport, IconSettings } from '@tabler/icons-react';
-import { IconUsers, IconUser, IconWorldUpload, IconFileDescription, IconMathFunction } from '@tabler/icons-react';
+import { IconFileExport, IconSettings, IconUsers, IconUser, IconWorldUpload, IconFileDescription, IconMathFunction, IconSchool, IconApps,IconCertificate,IconBrandMessenger } from '@tabler/icons-react';
 import { useContext, useState } from 'react';
 
 import { useTranslation } from 'next-i18next';
@@ -13,6 +11,7 @@ import { StudentDialog } from '@/components/Settings/StudentDialog';
 import { SearchDialog } from '@/components/Settings/SearchDialog';
 import { PaperDialog } from '@/components/Settings/PaperDialog';
 import { CalculateDialog } from '@/components/Settings/CalculateDialog';
+import { TechnologyDialog } from '@/components/Settings/TechnologyDialog';
 
 import { Import } from '../../Settings/Import';
 import { Key } from '../../Settings/Key';
@@ -29,6 +28,12 @@ export const ChatbarSettings = () => {
   const [isSearchDialogOpen, setIsSearchDialog] = useState<boolean>(false);
   const [isPaperDialogOpen, setIsPaperDialog] = useState<boolean>(false);
   const [isCalculateDialogOpen, setIsCalculateDialog] = useState<boolean>(false);
+  const [isTechnologyDialogOpen, setIsTechnologyDialog] = useState<boolean>(false);
+
+  const [isCampusAssistantVisible, setIsCampusAssistantVisible] = useState(false);
+  const [isShowSubButtons, setShowSubButtons] = useState(false);
+  const [isShowTechnologyButton, setShowTechnologyButton] = useState(false);
+
 
   const {
     state: {
@@ -48,6 +53,18 @@ export const ChatbarSettings = () => {
     handleApiKeyChange,
     // handleTeachingAssistant,
   } = useContext(ChatbarContext);
+
+  const handleToggleCampusAssistant = () => {
+    setIsCampusAssistantVisible(!isCampusAssistantVisible);
+  };
+
+  const handleToggleShowSubButtons = () => {
+    setShowSubButtons(!isShowSubButtons);
+  };
+
+  const handleToggleShowTechnologyButton = () => {
+    setShowTechnologyButton(!isShowTechnologyButton);
+  };
 
   return (
     <div className="flex flex-col items-center space-y-1 border-t border-white/20 pt-1 text-sm">
@@ -69,46 +86,95 @@ export const ChatbarSettings = () => {
         onClick={() => setIsSettingDialog(true)}
       />
 
-      {/* 教师助理 按钮渲染 */}
+      {/* 校园助理按钮渲染 */}
       <SidebarButton
-        text={t('教师助理')}
-        icon={<IconUsers  size={18} />}
-        onClick={() => setIsTeacherDialog(true)}
+        text={t('校园助理')}
+        icon={<IconSchool size={18} />}
+        onClick={handleToggleCampusAssistant}
       />
 
-      {/* 学生助理 按钮渲染 */}
+      {isCampusAssistantVisible && (
+        <>
+          <div>
+            {/* 教师助理按钮渲染 */}
+            <SidebarButton
+              text={t('教师助理')}
+              icon={<IconUsers size={18} />}
+              onClick={() => setIsTeacherDialog(true)}
+            />
+          </div>
+          <div>
+            {/* 学生助理按钮渲染 */}
+            <SidebarButton
+              text={t('学生助理')}
+              icon={<IconUser size={18} />}
+              onClick={() => setIsStudentDialog(true)}
+            />
+          </div>
+        </>
+      )}
+
+      {/* 智能插件按钮 */}
       <SidebarButton
-        text={t('学生助理')}
-        icon={<IconUser  size={18} />}
-        onClick={() => setIsStudentDialog(true)}
+        text={t('智能插件')}
+        icon={<IconApps size={18} />}
+        onClick={handleToggleShowSubButtons}
       />
 
-      {/* 联网搜索 按钮渲染 */}
+      {/* 显示三个子按钮 */}
+      {isShowSubButtons && (    
+        <>
+          <div className="ml-2">
+            {/* 联网搜索按钮 */}
+            <SidebarButton
+              text={t('联网搜索')}
+              icon={<IconWorldUpload size={18} />}
+              onClick={() => setIsSearchDialog(true)}
+            />
+          </div>
+          <div className="ml-2">
+            {/* 论文检索按钮 */}
+            <SidebarButton
+              text={t('论文检索')}
+              icon={<IconFileDescription size={18} />}
+              onClick={() => setIsPaperDialog(true)}
+            />
+          </div>
+          <div className="ml-2">
+            {/* 数学计算按钮 */}
+            <SidebarButton
+              text={t('数学计算')}
+              icon={<IconMathFunction size={18} />}
+              onClick={() => setIsCalculateDialog(true)}
+            />
+          </div>
+        </>
+      )}
+
+      {/* 课程助手按钮 */}
       <SidebarButton
-        text={t('联网搜索')}
-        icon={<IconWorldUpload  size={18} />}
-        onClick={() => setIsSearchDialog(true)}
+        text={t('课程助手')}
+        icon={<IconCertificate size={18} />}
+        onClick={handleToggleShowTechnologyButton}
       />
 
-      {/* 论文检索 按钮渲染 */}
-      <SidebarButton
-        text={t('论文检索')}
-        icon={<IconFileDescription  size={18} />}
-        onClick={() => setIsPaperDialog(true)}
-      />
+      {/* 显示“开源软件开发技术”按钮 */}
+      {isShowTechnologyButton && (
+        <div className="ml-2">
+          {/* 开源软件开发技术按钮 */}
+          <SidebarButton
+            text={t('开源软件开发技术')}
+            icon={<IconBrandMessenger size={18} />}
+            onClick={() => setIsTechnologyDialog(true)}
+          />
+        </div>
+      )}
 
-      {/* 数学计算 按钮渲染 */}
-      <SidebarButton
-        text={t('数学计算')}
-        icon={<IconMathFunction  size={18} />}
-        onClick={() => setIsCalculateDialog(true)}
-      />
-
-      {!serverSideApiKeyIsSet ? (
+      {/* {!serverSideApiKeyIsSet ? (
         <Key apiKey={apiKey} onApiKeyChange={handleApiKeyChange} />
       ) : null}
 
-      {!serverSidePluginKeysSet ? <PluginKeys /> : null}
+      {!serverSidePluginKeysSet ? <PluginKeys /> : null} */}
 
       <SettingDialog
         open={isSettingDialogOpen}
@@ -116,6 +182,7 @@ export const ChatbarSettings = () => {
           setIsSettingDialog(false);
         }}
       />
+   
       <TeacherDialog
         open={isTeacherDialogOpen}
         onClose={() => {
@@ -145,7 +212,13 @@ export const ChatbarSettings = () => {
         onClose={() => {
           setIsCalculateDialog(false);
         }}
-      />         
+      />    
+      <TechnologyDialog
+        open={isTechnologyDialogOpen}
+        onClose={() => {
+          setIsTechnologyDialog(false);
+        }}
+      />          
     </div>
   );
 };
